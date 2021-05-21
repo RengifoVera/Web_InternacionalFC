31d2284be5b572c9b57090d7a5213968a9a60a8b
Jhon Vera <rengifovera162@gmail.com>
2021-05-21 18:25:35 -0500

    Intento

---
+++ Web_InternacionalFC/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProyectoWeb_InternacionalFCConfig(AppConfig):
+    name = 'Web_InternacionalFC'
---
+++ Web_InternacionalFC/static/js/nav-bar.js
@@ -0,0 +1,10 @@
+const ham = document.querySelector('.ham')
+const enlaces =document.querySelector('.enlaces-menu')
+const baras=document.querySelectorAll('.ham span')
+
+ham.addEventListener('click',()=>{
+    enlaces.classList.toggle('activado');
+    baras.forEach(child => {child.classList.toggle('animado')});
+});
+
+gsap.from("#logo", {duration: 3, x: 300, opacity: 0, scale: 0.5});
