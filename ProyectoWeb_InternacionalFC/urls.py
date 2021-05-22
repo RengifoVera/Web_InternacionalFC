80f7cabcc5693112d8e8c7a442d040d525c72536
Jhon Vera <rengifovera162@gmail.com>
2021-05-22 14:50:28 -0500

    Creacion seccion Experiencia

--- ProyectoWeb_InternacionalFC/urls.py
+++ ProyectoWeb_InternacionalFC/urls.py
@@ -20,4 +20,6 @@ from Web_InternacionalFC import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name="home"),
+    path('experiencia/',views.experiencia,name="experiencia"),
+    path('index/', views.index,name="index"),
 ]
