6dd353ed54c028586abbc3c8fbb5326a21b543e8
Jhon Vera <rengifovera162@gmail.com>
2021-05-25 20:35:49 -0500

    Seccion experiencia

--- Web_InternacionalFC/static/js/nav-bar.js
+++ Web_InternacionalFC/static/js/nav-bar.js
@@ -9,3 +9,10 @@ ham.addEventListener('click',()=>{
 
 gsap.from("#logo", {duration: 3, x: 300, opacity: 0, scale: 0.5});
 
+let tween = gsap.from('.circle', {
+            duration: .3,
+            y: -200, //'random(-500,500)',
+            scale: 0,
+            stagger: 0.5,
+            //delay: 2 // it's better to use timeline
+        });
